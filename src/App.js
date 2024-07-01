3507fbabac232ab366fa299d1799c264b757642a
GKavvadias <il_gato_nero@hotmail.com>
2024-07-01 14:41:54 +0300

    Adding Loader, Error and StartScreen components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
-  question: [],
+  questions: [],
 
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: "loading",
@@ -12,7 +15,7 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case "dataReceived":
-      return { ...state, question: action.payload, status: "ready" };
+      return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
       return { ...state, status: "error" };
     default:
@@ -21,7 +24,9 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -34,8 +39,8 @@ export default function App() {
     <div className="app">
       <Header />
       <Main className="main">
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />} {status === "error" && <Error />}{" "}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
